63aa27a8777003fd4d419e08bb4d4794039b4dfb
Vamsi Eyunni <vamsieyunni@gmail.com>
2026-05-29 02:46:51 -0700

    add baseline script 1 with safety filter =true

--- eval_double_integrator.sh
+++ eval_double_integrator.sh
@@ -4,7 +4,7 @@ experiment_name_str="double_integrator_safety_informed"
 # "double_integrator" or "airtaxi"
 dynamics_type="double_integrator"
 # for custom scenario, use "navigation_graph_safe_eval" and check the last line of multiagent/config.py to see what custom scenario is used
-scenario_name="navigation_graph_safe"
+scenario_name="navigation_graph_safe_eval"
 use_safety_filter="True"
 world_size=4
 # Read last line of config.py and echo
@@ -12,17 +12,17 @@ config_file="multiagent/config.py"
 original_string=$(tail -n 1 $config_file)
 model_dir_str="trained_models/${experiment_name_str}"
 
-num_eval_episodes=3
-num_eval_agents=4
+num_eval_episodes=10
+num_eval_agents=6
 eval_episode_length=250
 echo "Running ${num_eval_episodes} episodes with ${num_eval_agents} agents and episode length ${eval_episode_length}."
 
 python scripts/eval_mpe.py \
 --model_dir=${model_dir_str} \
 --dynamics_type ${dynamics_type} --render_episodes=${num_eval_episodes} \
---world_size=${world_size} --num_landmarks=2 \
+--world_size=${world_size} --num_landmarks=3 \
 --num_agents=${num_eval_agents} \
---num_obstacles=0 \
+--num_obstacles=3 \
 --seed=0 \
 --episode_length=${eval_episode_length} \
 --use_dones=False --collaborative=False \
